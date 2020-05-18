5ca7dfd9d2e20cbc5cd7cc35692baf6723d82052
LukeRamsay <43437616+LukeRamsay@users.noreply.github.com>
2020-05-18 13:35:40 +0200

    Initial Commit

---
+++ MugRecipies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MugRecipies
+//
+//  Created by Luke Ramsay on 2020/05/18.
+//  Copyright © 2020 Luke Ramsay. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
