b9c5c00952e296b99d9d8e8c6b74b5e17d3eae77
LukeRamsay <43437616+LukeRamsay@users.noreply.github.com>
2020-05-25 17:20:34 +0200

    Added Custom Trigger
    added a working custom trigger and a useable egg usdz file

--- MugRecipies/ViewController.swift
+++ MugRecipies/ViewController.swift
@@ -12,6 +12,8 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var textPannel: UIVisualEffectView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +23,27 @@ class ViewController: UIViewController {
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        //Custom
+        
+        boxAnchor.actions.showInterface.onAction = handleShowInterface(_:)
+    }
+    
+    //MARK: - Custom Behaviour
+    
+    func handleShowInterface(_ entity: Entity?){
+        guard entity != nil else {return}
+        
+        print("Scene Start")
+        
+        textPannel.isHidden = false
+        showButton.isHidden = false
+    }
+    
+    @IBAction func OnShowIngredientsTapped(_ sender: Any) {
+        if let sceneAnchor = arView.scene.anchors[0] as? Experience.Box {
+        
+        sceneAnchor.notifications.showIngredients.post()
+        }
     }
 }
